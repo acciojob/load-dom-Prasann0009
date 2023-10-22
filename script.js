6cf1ddda237d4dffa14288c0db27d5422ec8bd08
Prasann0009 <130238044+Prasann0009@users.noreply.github.com>
2023-10-22 10:10:14 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
 //your JS code here. If required.
+  function onDOMLoad() {
+            // Get the body element
+            var body = document.body;
+
+            // Clear any existing content in the body
+            body.innerHTML = "";
+
+            // Create a new text node with the desired message
+            var textNode = document.createTextNode("DOM load success");
+
+            // Append the text node to the body
+            body.appendChild(textNode);
+        }
+
+        // Add an event listener for the DOMContentLoaded event
+        document.addEventListener("DOMContentLoaded", onDOMLoad);
